d1c17322b4a5811b72ef811cf74d54578b1f0e93
yanmengfei <yanmengfei@inspur.com>
2020-06-25 17:04:49 +0800

    1、详情页样式调整 2、页面顶部标签跳转传参

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  resourceURL = 'http://www.ypppt.com/uploads/allimg/200611/';
-  appURL = 'http://localhost:4200/app/detail/';
   title = 'ocean-web';
-   navigationBarList = [
+  navigationBarList = [
     'PPT模板'
     , 'PPT背景'
     , 'PPT图表'
@@ -26,33 +24,7 @@ export class AppComponent {
     '友链申请' ,
     '网站地图' ,
   ];
-  /*展示模块的控制变量*/
-  showNavMap: { [name: string]: boolean } = {
-    showTemplate: true
-    , showBackground: false
-  };
 
   constructor() {
-  }
-
-  menuSelected() {
-    console.log('selected');
-  }
-
-  /*  */
-  showHandler(nav: string) {
-    for (const key in this.showNavMap) {
-      if (key !== nav) {
-        this.showNavMap[key] = false;
-      } else {
-        this.showNavMap[key] = true;
-      }
-    }
-  }
-
-  /*新页签展示详情页*/
-  openDetailPage(url: string) {
-    window.open('#/powerpoint-detail');
-    // this.router.navigateByUrl('/powerpoint-detail');
   }
 }
