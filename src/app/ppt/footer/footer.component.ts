70aac443681ae11e27d573af8189e8be076ba9f6
sunbin.lc <sunbin.lc@inspur.com>
2020-07-11 15:11:13 +0800

    add footer

--- src/app/ppt/footer/footer.component.ts
+++ src/app/ppt/footer/footer.component.ts
@@ -1,7 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
 import {PowerPointService} from '../../../service/PowerPointService';
+import {$} from 'protractor';
 
 @Component({
   selector: 'app-detail-component',
@@ -13,13 +14,25 @@ export class FooterComponent implements OnInit{
 
   mode = false;
   dark = true;
+  barName = '';
   constructor(private route: ActivatedRoute,
               private http: HttpClient,
-              private powerPointService: PowerPointService) {
+              private powerPointService: PowerPointService,
+              private elementRef: ElementRef) {
   }
   ngOnInit(): void {
     this.route.queryParams.subscribe((data) => {
+      this.barName = data.bar_name;
+      console.log(this.barName);
     });
   }
 
+  transferContent(content: string) {
+    this.barName = content;
+    // document.getElementsByTagName('body').scrollTop(0);
+    // this.elementRef.nativeElement('#page').animate( {scrollTop: 0}, 500);
+    document.documentElement.scrollIntoView();
+
+
+  }
 }
